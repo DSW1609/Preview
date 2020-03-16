89526b1c822144ee42ea8d696cb4d7c4137d7b2e
DSW1609 <DSW7296666@163.com>
2020-03-16 08:53:04 +0800

    UP UP DSW

--- zajindan/js/zajindan.js
+++ zajindan/js/zajindan.js
@@ -1,10 +1,10 @@
 $(function () {
-  // 定义抽奖次数
+  // 初始化抽奖次数
   var Jh_I = 1;
   // 把次数输出到页面
   $("#Jh_I").text(Jh_I);
-  // 定义抽奖信息
-  var XX = [{
+  // 初始化奖品内容
+  var JDD_XX = [{
       // 标题
       Tit: '中奖了',
       // 信息
@@ -82,12 +82,12 @@ $(function () {
       // 页面禁止滚动
       $("body").css("overflow", "hidden");
       // 获得随机数
-      var SJX = Math.floor(Math.random() * XX.length);
+      var SJX = Math.floor(Math.random() * JDD_XX.length);
       // 添加抽奖信息
       // 信息标题
-      $(".Xx_Tt").text(XX[SJX].Tit);
+      $(".Xx_Tt").text(JDD_XX[SJX].Tit);
       // 信息信息
-      $(".Xx_Xx").text(XX[SJX].Xx);
+      $(".Xx_Xx").text(JDD_XX[SJX].Xx);
       setTimeout(function () {
         // 更改金蛋蛋图片
         $(".JDD").addClass("JDDS");
@@ -96,7 +96,7 @@ $(function () {
         // 显示抽奖信息
         $(".Cj_Ctt").css('transform', 'scale(1.2)');
         // 如果中奖了下方打印信息
-        if (XX[SJX].Tit == "中奖了") {
+        if (JDD_XX[SJX].Tit == "中奖了") {
           // 更改下方按钮文字
           $(".Xx_Btn").html('<a id="Z_Sre">去分享</a>');
           //判断是否在前面加0
@@ -111,7 +111,7 @@ $(function () {
           var Z_now = Z_year + '.' + getNow(Z_mon) + "." + getNow(Z_date);
           // 打印
           $(".ZJXX").prepend('<div class="XX_Item">' +
-            XX[SJX].Xx + '<time>' + Z_now + '</time>' +
+            JDD_XX[SJX].Xx + '<time>' + Z_now + '</time>' +
             '</div>')
         } else {
           // 底部按钮
@@ -176,14 +176,14 @@ $(function () {
       $(".G_Te_Ctt").css("transition", '1.5s');
     }
   }, 3000);
-  // 活动规则/中奖信息按钮点击事件
+  // 活动规则/中奖记录按钮点击事件
   function C_Btn(TC) {
     // 显示弹窗
     $(TC).css("transform", "scale(1)")
     // 禁止页面滚动
     $("body").css("overflow", "hidden");
   }
-  // 活动规则/中奖信息部分关闭按钮点击事件
+  // 活动规则/中奖记录部分关闭按钮点击事件
   function C_Gb(Y_TC) {
     // 隐藏弹窗
     $(Y_TC).css("transform", "scale(0)")
@@ -200,18 +200,20 @@ $(function () {
   $("#Yc_Gb").click(function () {
     C_Gb(".C_L_Ctt");
   })
-  // 中奖信息点击事件
+  // 中奖记录点击事件
   $(".C_Btn_R").click(function () {
     C_Btn(".Zj_Xx_Ctt");
   })
-  // 中奖信息关闭按钮点击事件
+  // 中奖记录关闭按钮点击事件
   $("#Zj_Gb").click(function () {
     C_Gb(".Zj_Xx_Ctt");
   })
+
   // 去完成按钮点击事件
   $(".Rw_An_Qw").click(function () {
     // 判断是否完成任务
     var Rw = true;
+    // 如果完成
     if (Rw) {
       // 更改按钮为领取样式
       $(this).addClass("Rw_An_Lq");
@@ -219,27 +221,34 @@ $(function () {
       $(this).text("领取");
     }
   })
+  // 初始化领取按钮增加的次数
+  var An_Cs = [1, 1, 3, 2, 5];
   // 领取按钮点击事件(一次)
   $(".Rw_An_Lq").one("click", function () {
     // 添加已完成样式
     $(this).addClass("Rw_An_Wc");
     // 更改文字为已领取
     $(this).text("已领取");
+    // 判断点击的是第几个按钮
+    var An_In = $(".Rw_An").index(this);
     // 增加砸金蛋次数
-    Jh_I += parseInt($(this).attr('data-R'));
+    Jh_I += An_Cs[An_In];
+    // 输出到页面
     $("#Jh_I").text(Jh_I);
   })
   // 判断是否为新用户,显示弹窗
-  // 清除
   if (localStorage.getItem('isNew') == null) {
     $(".Z_New").css("transform", "scale(1)");
+    // 禁止页面滚动
     $("body").css("overflow", "hidden");
   }
   // 关注公众号弹窗按钮点击事件
   $(".Z_New_Btn").click(function () {
     // 隐藏关注弹窗
     $(".Z_New").css("transform", "scale(0)");
+    // 恢复页面滚动
     $("body").css("overflow", "auto");
+    // 存储点击按钮记录
     localStorage.setItem('isNew', true);
   })
 })
